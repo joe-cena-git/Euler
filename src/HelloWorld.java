9b8c6623819981757759e0df2a880e581880dcff
Joe Cenatiempo <joecenatiempo@gmail.com>
2015-06-09 11:49:33 -0400

    Add Java Project and Hello World

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * @author jcenatiempo
+ * HelloWorld.java
+ * Simple Hello World class to get things started.
+ */
+
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+	}
+}
